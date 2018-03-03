c8452d0a2ccf0289cd4c75e97b12f726cdf574ae
Arnau Blanco <arnau.blanco01@estudiant.upf.edu>
2018-03-03 19:10:38 +0100

    comments to understand it better

--- database.c
+++ database.c
@@ -4,26 +4,25 @@
 
 #include "database.h"
 
-
 node_t* new_node(int **mat,int D,int org,int dest)
 {
-    //1. Load the previous matrix => DONE
-    //2. Check who's the top in towerorg
-    int toporg;
+    //Define the variables that will store the tops in tower origin and tower destination
+    int toporg, topdest;
+    //Find the top in tower origin
     for(int i=D-1; i>=0; i--)
+        //If position i in that matrix is equal to 0, it means in the previous position there is the top
         if(mat[org][i]==0){
-            toporg = i-1;
             break;
         }else{
             toporg = i;
         }
-    //3. Find empty position in towerdest
-    int topdest;
+    //Find the top in tower destination
     for(int i=D-1; i>=0; i--)
+        //If position i equals to 0, then there is an empty space
         if(mat[dest][i]==0)
             topdest = i;
-            break;
-    //4. Fill the matrix
+            break;           
+    //Fill those positions with their corresponding values
     mat[dest][topdest] = mat[org][toporg];
     mat[org][toporg] = 0;
 }
